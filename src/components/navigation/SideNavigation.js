4db45058c0e40dc59dd4dc48054b7202ba994426
Gambit142 <nzefrancismaryeugorji@gmail.com>
2023-08-19 11:12:20 +0100

    Create Navigation links and Side Bar

---
+++ src/components/navigation/SideNavigation.js
@@ -0,0 +1,30 @@
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import Navigation from './Navigation';
+
+const SideNavigation = ({ navigationLinksData }) => (
+  <section>
+    <Link to="/home" className="logo">
+      <span className="upper-logo-text">Paybox</span>
+      <span className="lower-logo-text">by LibertyPay</span>
+    </Link>
+    <Navigation navigationLinksData={navigationLinksData} />
+  </section>
+);
+
+SideNavigation.displayName = 'SideNavigation';
+
+SideNavigation.propTypes = {
+  navigationLinksData: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      icon: PropTypes.string.isRequired,
+      path: PropTypes.string.isRequired,
+      alt: PropTypes.string.isRequired,
+      rightIcon: PropTypes.string, // Optional
+      component: PropTypes.elementType.isRequired,
+    }),
+  ).isRequired,
+};
+
+export default SideNavigation;
