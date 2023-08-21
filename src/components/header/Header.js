30a14704ee208ada8f0c379724d29acd26ac4963
Gambit142 <nzefrancismaryeugorji@gmail.com>
2023-08-21 11:32:10 +0100

    Modify Header component to suit design

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,33 +1,62 @@
+import { useState } from 'react';
 import MediaQuery from 'react-responsive';
 import PropTypes from 'prop-types';
+import { slide as Menu } from 'react-burger-menu';
+import MobileNavigation from '../navigation/MobileNavigation';
+import styles from './header.module.css';
 
-const Header = ({ title }) => (
-  <header>
-    <MediaQuery maxWidth={768}>
-      <h1>{title}</h1>
-      <button type="button">
-        Menu
-      </button>
-    </MediaQuery>
-    <MediaQuery minWidth={769}>
-      <div>
-        <h1>{title}</h1>
-        <div>
-          <img src="/assets/images/notification-bing.svg" alt="notification icon" />
-          <img src="/assets/images/Francis_Ugorji.jpg" alt="user icon" />
+const Header = ({ title }) => {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
+  const mobileMenuStateChangeHandler = ({ isOpen }) => {
+    setIsMobileMenuOpen(isOpen);
+  };
+
+  const closeMobileMenu = () => {
+    setIsMobileMenuOpen(false);
+  };
+
+  return (
+    <section>
+      <MediaQuery maxWidth={768}>
+        <header className={`flex secondary-background ${styles.header}`}>
+          <h1 className="white nunito-font-500 line-height-1">{title}</h1>
+          <button type="button" onClick={toggleMobileMenu} className="white montserrat-font-500 fs-14 line-height-1">
+            Menu
+          </button>
+        </header>
+        <Menu
+          isOpen={isMobileMenuOpen}
+          onStateChange={mobileMenuStateChangeHandler}
+        >
+          <MobileNavigation closeMobileMenu={closeMobileMenu} />
+        </Menu>
+      </MediaQuery>
+      <MediaQuery minWidth={769}>
+        <header>
           <div>
-            <h2>Francis Ugorji</h2>
+            <h1>{title}</h1>
             <div>
-              <span>nzefrancismaryeugorji@gmail.com</span>
-              <img src="/assets/images/Chevron-down.svg" alt="chevron down icon" />
+              <img src="/assets/images/notification-bing.svg" alt="notification icon" />
+              <img src="/assets/images/Francis_Ugorji.jpg" alt="user icon" />
+              <div>
+                <h2>Francis Ugorji</h2>
+                <div>
+                  <span>nzefrancismaryeugorji@gmail.com</span>
+                  <img src="/assets/images/Chevron-down.svg" alt="chevron down icon" />
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-      </div>
-
-    </MediaQuery>
-  </header>
-);
+        </header>
+      </MediaQuery>
+    </section>
+  );
+};
 
 Header.displayName = 'Header';
 
