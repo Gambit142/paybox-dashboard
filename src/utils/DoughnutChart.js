6d0c822bf6b6a3434b7815c8c6af1f5debd05cc0
Gambit142 <nzefrancismaryeugorji@gmail.com>
2023-08-22 20:11:24 +0100

    Create DoughnutChart component to be used

---
+++ src/utils/DoughnutChart.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import {
+  PieChart, Pie, Cell, ResponsiveContainer,
+} from 'recharts';
+
+const data = [
+  { name: 'Label 1', value: 60 },
+  { name: 'Label 2', value: 20 },
+  { name: 'Label 3', value: 20 },
+];
+
+const COLORS = ['#032282', '#F4BE38', '#EDE1FF'];
+
+const renderCustomizedLabel = ({
+  cx, cy, midAngle, innerRadius, outerRadius, percent,
+}) => {
+  const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+  const x = cx + radius * Math.cos(-midAngle * (Math.PI / 180));
+  const y = cy + radius * Math.sin(-midAngle * (Math.PI / 180));
+  const circleRadius = 18;
+
+  return (
+    <g>
+      <circle
+        cx={x}
+        cy={y}
+        r={circleRadius}
+        fill="white"
+        fillOpacity="1"
+      />
+      <text
+        x={x}
+        y={y}
+        textAnchor="middle"
+        dominantBaseline="central"
+        fill="black"
+        fontSize="12px"
+        fontWeight="600"
+      >
+        {`${(percent * 100).toFixed(0)}%`}
+      </text>
+    </g>
+  );
+};
+
+const DoughnutChart = () => (
+  <ResponsiveContainer width="100%" height={300}>
+    <PieChart>
+      <Pie
+        data={data}
+        dataKey="value"
+        cx="25%"
+        cy="35%"
+        innerRadius={50}
+        outerRadius={75}
+        paddingAngle={5}
+        labelLine={false}
+        label={renderCustomizedLabel}
+      >
+        {data.map((entry, index) => (
+          <Cell key={`cell-${entry.name}`} fill={COLORS[index]} />
+        ))}
+      </Pie>
+    </PieChart>
+  </ResponsiveContainer>
+);
+
+export default DoughnutChart;
