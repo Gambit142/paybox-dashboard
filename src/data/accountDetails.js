d80e415a9378ce6f097e3910699cb717e920d2b9
Gambit142 <nzefrancismaryeugorji@gmail.com>
2023-08-22 08:10:46 +0100

    Create account details data for use in AccountSummary component

---
+++ src/data/accountDetails.js
@@ -0,0 +1,16 @@
+const accountDetails = [
+  {
+    detailType: 'Total savings balance',
+    detailAmount: '₦194,000',
+  },
+  {
+    detailType: 'Total customers',
+    detailAmount: '₦919',
+  },
+  {
+    detailType: 'Total POS transactions',
+    detailAmount: '0',
+  },
+];
+
+export default accountDetails;
